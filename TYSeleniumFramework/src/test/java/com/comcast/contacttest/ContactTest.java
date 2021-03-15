cdc29834d0addbf421632b793aa2d61aa72a530a
sumipriya <sumipriya@sumi>
2021-03-15 00:27:28 -0700

    commit

---
+++ TYSeleniumFramework/src/test/java/com/comcast/contacttest/ContactTest.java
@@ -0,0 +1,23 @@
+package com.comcast.contacttest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createcontact(){
+		System.out.println("execute createcontact");
+	}
+	
+	@Test
+	public void modifycontacttest(){
+		System.out.println("execute modifycontacttest");
+	}
+	
+	@Test
+	public void deletecontacttest(){
+		System.out.println("execute deletecontacttest");
+	}
+}
+
+
+//https://github.com/suma1996-star/SDET_11_Framework_BA.git
