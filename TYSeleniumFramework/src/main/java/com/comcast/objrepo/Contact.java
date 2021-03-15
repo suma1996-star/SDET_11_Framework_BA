644484d0f2fd38c78be18663cdbdfbf5108f32af
sumipriya <sumipriya@sumi>
2021-03-15 01:23:18 -0700

    Added Contact page

---
+++ TYSeleniumFramework/src/main/java/com/comcast/objrepo/Contact.java
@@ -0,0 +1,13 @@
+package com.comcast.objrepo;
+
+public class Contact {
+
+	public void createcontact(String mobilenumber){
+		System.out.println("execute createcontact");
+	}
+	
+	public void createcontact(String mobilenumber, String address,String name){
+		System.out.println("execute createcontact");
+	}
+	
+}
